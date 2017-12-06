9b10e4713449a54bb4f65f6881e5df55a71191d3
root <root@ubuntu.(none)>
2017-12-06 21:34:37 +0800

    Scott added the first time 4 files

---
+++ main.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 06 Dec 2017 09:27:40 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    printf("hello Git\n");
+    
+    return 0;
+}
